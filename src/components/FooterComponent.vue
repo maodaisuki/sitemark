5c654f3f52e4c102edd0aa21d77c1a473efa877c
maodaisuki <nyanyanyapu@gmail.com>
2023-11-23 14:42:41 +0800

    添加页脚信息

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -4,9 +4,16 @@
 <template>
   <div class="footer-container">
     <a-divider :size="1"/>
-    页脚信息
+    <div class="footer-info">
+      &copy; 2023 mao.&nbsp;&nbsp;Powered by <a-link style="color: #999;" herf="https://github.com/maodaisuki/sitemark">GithubPage.</a-link>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.footer-info {
+  color: #999;
+  font-size: 0.85rem;
+}
+
 </style>
